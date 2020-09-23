dc589ad0cb4e00275bd49f8515a194db06918215
Andrei Voicu <dreivoicu@gmail.com>
2020-09-23 20:19:35 +0300

    deploy test

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,17 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: (config) => {
+    if (config.mode === 'production') {
+      config.output.publicPath = 'vue-sb'
+    }
+
+    return config;
+  },
 }
